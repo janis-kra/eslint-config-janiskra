8dea914c4d5a7db9659c5cb7083fd0c5f23fce13
Janis Krasemann <janis.krasemann@enpit.de>
2016-07-27 11:43:09 +0200

    add empty env object and reconfigure two spacing rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
     "extends": "airbnb",
     "installedESLint": true,
-    "plugins": []
+    "plugins": [],
+    "env": {},
+    "rules": {
+        "space-before-function-paren": ["error", "always"],
+        "object-curly-spacing": ["error", "always"]
+    }
 };
