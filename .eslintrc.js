8a9f23c7acfa05bff88bd2bc3f5aeac883832f21
Janis Krasemann <janis.krasemann@enpit.de>
2016-08-03 21:21:05 +0200

    set ecmascript to v6, add plugin and some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,16 @@
 module.exports = {
     "extends": "airbnb",
     "installedESLint": true,
-    "plugins": [],
-    "env": {},
+    "plugins": [
+        "no-inferred-method-name"
+    ],
+    "settings": {
+        "ecmascript": 6
+    },
     "rules": {
         "space-before-function-paren": ["error", "always"],
-        "object-curly-spacing": ["error", "always"]
+        "object-curly-spacing": ["error", "always"],
+        "comma-dangle": ["error", "never"],
+        "object-shorthand": ["error", "never"]
     }
 };
