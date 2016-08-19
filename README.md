c5134869226c8e5a0a5b61a9ca5c5649b8b7da19
Janis Krasemann <janis.krasemann@enpit.de>
2016-08-20 01:19:10 +0200

    add async-await rules (which results in the babel-eslint parser being required)

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # eslint-config-janiskra
-My custom eslint rules
+
+My custom eslint rules, extending the [eslint-config-airbnb](https://www.npmjs.com/package/eslint-config-airbnb).
+
+These warnings regarding missing peer dependencies are fine, as I only use `airbnb-base`:
+
+```
+UNMET PEER DEPENDENCY eslint-plugin-jsx-a11y@2.0.1
+UNMET PEER DEPENDENCY eslint-plugin-react@5.2.2
+```
--- index.js
+++ index.js
@@ -2,15 +2,24 @@ module.exports = {
     "extends": "airbnb-base",
     "installedESLint": true,
     "plugins": [
+        "async-await",
         "no-inferred-method-name"
     ],
     "settings": {
         "ecmascript": 6
     },
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "esversion":6
+        }
+    },
     "rules": {
-        "space-before-function-paren": ["error", "always"],
-        "object-curly-spacing": ["error", "always"],
+        "async-await/space-after-async": ["error", "always"],
+        "async-await/space-after-await": ["error", "always"],
         "comma-dangle": ["error", "never"],
-        "object-shorthand": ["error", "never"]
+        "object-curly-spacing": ["error", "always"],
+        "object-shorthand": ["error", "never"],
+        "space-before-function-paren": ["error", "always"]
     }
 };
