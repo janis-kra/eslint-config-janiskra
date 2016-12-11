24ca4bc34cc38f8ff1c0662d4a8a9a872fa47506
Janis Krasemann <janis.krasemann@enpit.de>
2016-12-11 20:35:02 +0100

    Update react config - do not extend plugin:react/recommended - add default rules for plugin:react - configure react/jsx-filename-extension rule to also allow jsx in js files

--- react.js
+++ react.js
@@ -1,7 +1,16 @@
 module.exports = {
   extends: [
     'airbnb',
-    'eslint-config-janiskra/defaults',
-    'plugin:react/recommended'
-  ]
+    'eslint-config-janiskra/defaults'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }]
+  }
 };
