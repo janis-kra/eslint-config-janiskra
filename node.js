3a88ce44409cf706ba24ca894e85b969cb8259e0
Janis Krasemann <janis.krasemann@enpit.de>
2017-01-10 09:15:05 +0100

    allow extraneous dev dependencies if the file reqpresents a test

--- node.js
+++ node.js
@@ -3,5 +3,10 @@ module.exports = {
     'eslint-config-janiskra/base',
     'plugin:node/recommended'
   ],
-  plugins: ['node']
+  plugins: ['node'],
+  rules: {
+    'import/no-extraneous-dependencies': ['error', {
+      devDependencies: ['**/*test.js', '**/*spec.js']
+    }]
+  }
 };
